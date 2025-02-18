f2ad0d3bf86435904edd4a3b0f77c081899c755d
const <sangsoo2652@gmail.com>
2025-02-18 17:50:25 +0900

    Feat : Convert each function to class

--- LaneDetection/LaneDetectionModule.py
+++ LaneDetection/LaneDetectionModule.py
@@ -4,86 +4,91 @@ import LaneUtils as utils
 
 # Global Variable (for static)
 # curveList = np.zeros(5, dtype=np.int32)  # Empty integer array
-curveList = [np.int32(0)] * 5
 
 # Get lane curve's trend
-def getLaneCurve(img: cv2.typing.MatLike, curLane: np.uint8, targetLane: np.uint8, display: int=0):
-  imgCopy = img.copy()
-  imgResult = img.copy()
+class laneDetectionModule:
+  def __init__(self):
+    self.curveList = [np.int32(0)] * 5
 
-  ### STEP 1 : Find Lane & Binarization
-  imgThres = utils.threshold(img)
 
-  ### STEP 2 : Warping Lane Image
-  hT, wT, c = img.shape
+  def getLaneCurve(self, img: cv2.typing.MatLike, curLane: np.uint8, targetLane: np.uint8, display: int=0):
+    imgCopy = img.copy()
+    imgResult = img.copy()
 
-  # Determine which region to warp according to the direction
-  # Option 1 : Use 1 ROI & move to end of lane when targetLane =/= curLane
-  points = np.float32([[wT*0.25,hT*0.5],[wT*0.75,hT*0.5],[wT*0.2,hT],[wT*0.8,hT]] if targetLane == curLane \
-                  else [[wT*0.4,hT*0.5],[wT*0.8,hT*0.5],[wT*0.375,hT],[wT*0.875,hT]] if targetLane > curLane \
-                  else [[wT*0.2,hT*0.5],[wT*0.6,hT*0.5],[wT*0.125,hT],[wT*0.625,hT]])
+    ### STEP 1 : Find Lane & Binarization
+    imgThres = utils.threshold(img)
 
-  imgWarp = utils.warpImg(imgThres, points, wT, hT)
-  imgWarpPoints = utils.drawPoints(imgCopy, points)
+    ### STEP 2 : Warping Lane Image
+    hT, wT, c = img.shape
 
-  ### STEP 3 : Calculate Gradient of Lane(= Intensity of Curve)
-  # Get histogram that accumulates pixel in a column
-  if display > 1:
-    middlePoint, imgHist = utils.getHistogram(imgWarp, display=True, minPer=0.4, region=2)   # Center position for the current lane(bottom of image)
-    curveAveragePoint, imgHist = utils.getHistogram(imgWarp, display=True, minPer=0.2, region=1)       # Average position of nearby roads
-  else:
-    middlePoint = utils.getHistogram(imgWarp, minPer=0.4, region=2)   # Center position for the current lane(bottom of image)
-    curveAveragePoint = utils.getHistogram(imgWarp, minPer=0.2, region=1)       # Average position of nearby roads
-  curveRaw = curveAveragePoint - middlePoint if not (np.isnan(middlePoint) or np.isnan(curveAveragePoint)) else 0 # Raw target curve(biased) intensity
+    # Determine which region to warp according to the direction
+    # Option 1 : Use 1 ROI & move to end of lane when targetLane =/= curLane
+    points = np.float32([[wT*0.25,hT*0.5],[wT*0.75,hT*0.5],[wT*0.2,hT],[wT*0.8,hT]] if targetLane == curLane \
+                    else [[wT*0.4,hT*0.5],[wT*0.8,hT*0.5],[wT*0.375,hT],[wT*0.875,hT]] if targetLane > curLane \
+                    else [[wT*0.2,hT*0.5],[wT*0.6,hT*0.5],[wT*0.125,hT],[wT*0.625,hT]])
 
-  ### STEP 4 : Smoothing Curve Using LPF Filter
-  curve = utils.smoothingCurve(curveList, curveRaw)
+    imgWarp = utils.warpImg(imgThres, points, wT, hT)
+    imgWarpPoints = utils.drawPoints(imgCopy, points)
 
-  ### STEP 5 : Display
-  if display > 0:
-    imgInvWarp = utils.warpImg(imgWarp, points, wT, hT, inverse=True)
-    #imgInvWarp[0 : hT//3, 0 : wT] = 0   # Masking the top of inv Image
-    imgInvWarp = cv2.cvtColor(imgInvWarp, cv2.COLOR_GRAY2BGR)
-    
-    imgLaneColor = np.full_like(img, (0, 255, 0))
-    imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
-    
-    imgResult = cv2.addWeighted(imgResult, 1, imgLaneColor, 1, 0)
-    midY = 450
-
-    cv2.putText(imgResult, str(curve), (wT//2-80,85), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,0))
-    cv2.line(imgResult, (wT//2,midY), (wT//2+(curve*3),midY), (255,0,255), 5)
-    cv2.line(imgResult, (wT//2+(curve*3),midY-25), (wT//2+(curve*3),midY), (255,0,255), 5)
-
-    for x in range(-30, 30):
-      w = wT//20
-      cv2.line(imgResult, (w*x+curve//50, midY-10), (w*x+curve//50, midY+10), (0,0,255), 2)
-    
+    ### STEP 3 : Calculate Gradient of Lane(= Intensity of Curve)
+    # Get histogram that accumulates pixel in a column
     if display > 1:
-      imgStacked = utils.stackImage(0.7, ([img,imgWarpPoints,imgWarp],[imgHist,imgLaneColor,imgResult]))
-      cv2.imshow('ImageStack', imgStacked)
+      middlePoint, imgHist = utils.getHistogram(imgWarp, display=True, minPer=0.4, region=2)   # Center position for the current lane(bottom of image)
+      curveAveragePoint, imgHist = utils.getHistogram(imgWarp, display=True, minPer=0.2, region=1)       # Average position of nearby roads
     else:
-      cv2.imshow('Result', imgResult)
+      middlePoint = utils.getHistogram(imgWarp, minPer=0.4, region=2)   # Center position for the current lane(bottom of image)
+      curveAveragePoint = utils.getHistogram(imgWarp, minPer=0.2, region=1)       # Average position of nearby roads
+    curveRaw = curveAveragePoint - middlePoint if not (np.isnan(middlePoint) or np.isnan(curveAveragePoint)) else 0 # Raw target curve(biased) intensity
 
-  ### STEP 6 : Normalization
-  # Mapping [(-inf,-th],(-th,th),[th,inf)] -> [-1,0,1]
-  #curveThres = 10
-  #curve = curve if np.abs(curve >= curveThres) else 0
+    ### STEP 4 : Smoothing Curve Using LPF Filter
+    curve = utils.smoothingCurve(self.curveList, curveRaw)
 
-  return curve
+    ### STEP 5 : Display
+    if display > 0:
+      imgInvWarp = utils.warpImg(imgWarp, points, wT, hT, inverse=True)
+      #imgInvWarp[0 : hT//3, 0 : wT] = 0   # Masking the top of inv Image
+      imgInvWarp = cv2.cvtColor(imgInvWarp, cv2.COLOR_GRAY2BGR)
+      
+      imgLaneColor = np.full_like(img, (0, 255, 0))
+      imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
+      
+      imgResult = cv2.addWeighted(imgResult, 1, imgLaneColor, 1, 0)
+      midY = 450
+
+      cv2.putText(imgResult, str(curve), (wT//2-80,85), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,0))
+      cv2.line(imgResult, (wT//2,midY), (wT//2+(curve*3),midY), (255,0,255), 5)
+      cv2.line(imgResult, (wT//2+(curve*3),midY-25), (wT//2+(curve*3),midY), (255,0,255), 5)
+
+      for x in range(-30, 30):
+        w = wT//20
+        cv2.line(imgResult, (w*x+curve//50, midY-10), (w*x+curve//50, midY+10), (0,0,255), 2)
+      
+      if display > 1:
+        imgStacked = utils.stackImage(0.7, ([img,imgWarpPoints,imgWarp],[imgHist,imgLaneColor,imgResult]))
+        cv2.imshow('ImageStack', imgStacked)
+      else:
+        cv2.imshow('Result', imgResult)
+
+    ### STEP 6 : Normalization & Thresholding
+    # Mapping [(-inf,-th],(-th,th),[th,inf)] -> [-1,0,1] & Avoid Twerking when curve near zero
+    #curveThres = 10
+    #curve = curve if np.abs(curve >= curveThres) else 0
+
+    return curve
 
 ### Unit Test
 if __name__ == '__main__':
   idx = 9
   cap = cv2.VideoCapture(f'video/camera{idx}.mp4')
 
-  testWidth = 480
-  testHeight = 240
+  dsize = (480, 240)  # (Width, Height)
   frameCounter = 0
 
-  initialTrackbarVals = [testWidth//2-120, testHeight//2, 35, testHeight]
-  utils.initializeTrackbars(initialTrackbarVals)
+  # DEBUG
+  # initialTrackbarVals = [testWidth//2-120, testHeight//2, 35, testHeight]
+  # utils.initializeTrackbars(initialTrackbarVals)
 
+  laneModule = laneDetectionModule()
   while cv2.waitKey(1) != ord('q'):
     frameCounter += 1
     if cap.get(cv2.CAP_PROP_FRAME_COUNT) == frameCounter:
@@ -95,11 +100,10 @@ if __name__ == '__main__':
       print("FAIL")
       break
 
-    img = cv2.resize(frame, (testWidth,testHeight))
-    curve = getLaneCurve(img, curLane=1, targetLane=0, display=2)
-    
-    if frameCounter % 5 == 0:
-      print(curve)
+    img = cv2.resize(frame, dsize=dsize)
+    curve = laneModule.getLaneCurve(img, curLane=0, targetLane=0, display=2)
+    if frameCounter % 3 == 0:
+      print(curve, laneModule.curveList)
 
   cap.release()
   cv2.destroyAllWindows()
--- LaneDetection/WebcamModule.py
+++ LaneDetection/WebcamModule.py
@@ -1,38 +1,46 @@
 import cv2
 
-default_h, default_w = 480, 640
-def webcamInit(size=(default_w,default_h)):
-  cap = cv2.VideoCapture(1) # 외장카메라 ID index 값 필요
-  if not cap.isOpened():
-    print("Cannot open video device")
-    return cap
+dsize = (640, 480)  # (Width, Height)
 
-  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, default_h)
-  cap.set(cv2.CAP_PROP_FRAME_WIDTH, default_w)
-  return cap
+class webcamModule:
+  def __init__(self, size: tuple=dsize):
+    self.cap = cv2.VideoCapture(1) # 외장카메라 ID index 값 필요
+    if not cap.isOpened():
+      print("Cannot open video device")
+      return
+    
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, size[0])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, size[1])
 
-def captureImg(cap: cv2.VideoCapture, display: bool=False, size: tuple=(default_h,default_w)):
-  success, img = cap.read()
 
-  if success:
-    img = cv2.resize(img, size)
-    if display:
-      cv2.imshow('IMG', img)
+  def captureImg(cap: cv2.VideoCapture, display: bool=False, size: tuple=dsize):
+    success, img = cap.read()
 
-  return success, img
+    if success:
+      img = cv2.resize(img, size)
+      if display:
+        cv2.imshow('IMG', img)
+
+    return success, img
 
 ### Unit Test
 if __name__ == '__main__':
-  from LaneDetectionModule import getLaneCurve
+  from LaneDetectionModule import laneDetectionModule
 
-  cap = webcamInit()
+  #cap = webcamModule()
+  cap = cv2.VideoCapture(f'video/camera9.mp4')
+  laneModule = laneDetectionModule()
+
   while cap.isOpened():
-    success, img = captureImg(cap, display=True)
+    #success, img = cap.captureImg(cap, display=True)
+    success, img = cap.read()
+    
     if not success:
       print("Video Capture Failed")
-      continue
+      break
 
-    curveVal = getLaneCurve(img, display=2)
+    img = cv2.resize(img, dsize=dsize)
+    curveVal = laneModule.getLaneCurve(img, curLane=0, targetLane=0, display=1)
     print(curveVal)
     
     if cv2.waitKey(1) == ord('q'):
